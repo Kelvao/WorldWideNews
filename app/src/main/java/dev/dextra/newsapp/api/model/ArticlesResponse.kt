fa1c79440e36d11181239375675578f4a2b2721e
Kelvao <kelvin.ievenes@fatec.sp.gov.br>
2019-07-22 20:16:27 -0300

    Feat: corrected and improved code

--- app/src/main/java/dev/dextra/newsapp/api/model/ArticlesResponse.kt
+++ app/src/main/java/dev/dextra/newsapp/api/model/ArticlesResponse.kt
@@ -1,12 +1,8 @@
 package dev.dextra.newsapp.api.model
-import com.google.gson.annotations.SerializedName
 
 
 data class ArticlesResponse(
-    @SerializedName("articles")
     val articles: List<Article>,
-    @SerializedName("status")
     val status: String,
-    @SerializedName("totalResults")
     val totalResults: Int
 )
--- app/src/main/java/dev/dextra/newsapp/api/model/SourceResponse.kt
+++ app/src/main/java/dev/dextra/newsapp/api/model/SourceResponse.kt
@@ -1,9 +1,6 @@
 package dev.dextra.newsapp.api.model
-import com.google.gson.annotations.SerializedName
 
 data class SourceResponse(
-    @SerializedName("sources")
     val sources: List<Source>,
-    @SerializedName("status")
     val status: String
 )
